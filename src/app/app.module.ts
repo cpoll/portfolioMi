5a275bd082ce2ca9d206003416f6b0e9d2f486bb
Cristian Poll <cpoll24@gmail.com>
2017-06-19 21:31:22 -0400

    add photos route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PhotosComponent } from './photos/photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'photo',
+        component: PhotosComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
